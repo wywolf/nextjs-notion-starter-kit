f16ab1a348c20367a94baef6423848b7c0c822df
wywolf <wangxiaoyan120@vip.163.com>
2024-01-11 13:53:16 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '6505727a301740a89e360fe7686e12d5',
+  rootNotionPageId: 'Leo-6505727a301740a89e360fe7686e12d5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -11,7 +11,7 @@ export default siteConfig({
   // basic site info (required)
   name: '效率工具分享',
 //   domain: 'https://www.notion.so/6505727a301740a89e360fe7686e12d5',
-  domain: 'https://erratic-firewall-fad.notion.site/6505727a301740a89e360fe7686e12d5',
+  domain: 'https://erratic-firewall-fad.notion.site/Leo-6505727a301740a89e360fe7686e12d5',
   author: 'Leo wang',
 
   // open graph metadata (optional)
